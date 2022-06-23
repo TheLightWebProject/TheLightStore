d5ec67135e6f823c268f22046672ea1e119fc25f
quangndgcc200030 <91590378+quangndgcc200030@users.noreply.github.com>
2022-06-23 08:49:03 +0700

    add payment now feature

--- src/Controller/OrderController.php
+++ src/Controller/OrderController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\OrderDetails;
+use App\Entity\Orders;
 use App\Repository\CustomersRepository;
+use App\Repository\OrdersRepository;
+use App\Repository\ProductsRepository;
 use App\Repository\UserRepository;
 use Doctrine\Persistence\ManagerRegistry;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -16,7 +20,7 @@ class OrderController extends AbstractController
     /**
      * @Route("/order/buynow", name="buynow")
      */
-    public function orderAction(Request $req, ManagerRegistry $re, UserRepository $repoUser, CustomersRepository $repoCus, AuthenticationUtils $authenticationUtils): Response
+    public function orderAction(Request $req, ManagerRegistry $res, UserRepository $repoUser, CustomersRepository $repoCus, OrdersRepository $repoOrder, ProductsRepository $repoPro, AuthenticationUtils $authenticationUtils): Response
     {
         $lastUsername = $authenticationUtils->getLastUsername();
         $user = $repoUser->findBy(['email' => $lastUsername]);
@@ -31,6 +35,45 @@ class OrderController extends AbstractController
         $image = $req->request->get('image');
         $quantity = $req->request->get('quantity');
         $price = $req->request->get('price');
+
+        if (isset($_POST["btnPaymentnow"])) {
+            //add order
+            $customerID = $repoCus->find($customer->getId()); //get entity customer
+
+            $order = new Orders();
+            $order->setOrderDate(new \DateTime());
+            $order->setDeliveryDate(new \DateTime());
+            $order->setChecked(false);
+            $order->setUsername($customerID);
+
+            $entity =  $res->getManager();
+            $entity->persist($order);
+            $entity->flush();
+
+            //add order details
+            $id = $req->request->get('proid');
+            $price = $req->request->get('price');
+            $product = $repoPro->find($id); //get entity product
+            $orderID = $repoOrder->find($order->getId()); //get entity order
+
+            $orderDetail = new OrderDetails();
+            $orderDetail->setQuantity($quantity);
+            $orderDetail->setTotalPrice($quantity * $price);
+            $orderDetail->setOrders($orderID);
+            $orderDetail->setProduct($product);
+
+            $entity->persist($orderDetail);
+            $entity->flush();
+
+            //update product
+            $quantity = $req->request->get('quantity');
+
+            $product->setQuantity($product->getQuantity() - $quantity);
+            $entity->persist($product);
+            $entity->flush();
+
+            return $this->redirectToRoute('shop');
+        }
 
         $product = [$id, $name, $short, $image, $quantity, $price];
 
