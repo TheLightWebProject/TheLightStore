d5bafb25ff5cfa454dc6e780a5c5b29f6a5653ca
quangndgcc200030 <91590378+quangndgcc200030@users.noreply.github.com>
2022-07-12 16:07:50 +0700

    Fix sql injection

--- public/index.php
+++ public/index.php
@@ -5,5 +5,8 @@ use App\Kernel;
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
+    if (session_status() === PHP_SESSION_NONE) {
+        session_start();
+    }
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
